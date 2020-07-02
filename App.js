8e33a721174e557eca3cf9e1a99a78fa6a1367d8
Anh Nguyen <phuonganhnguyenthi26.3@gmail.com>
2020-07-02 16:55:03 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+import {Button, View, Text} from 'react-native';
+import Header from './components/Header';
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  return (
+    <View>
+      <Header title="Counting app" />
+      <Text>{counter}</Text>
+      <Button title="Increment by 1" onPress={() => setCounter(counter + 1)} />
+      <Text>Current count is {counter}</Text>
+    </View>
+  );
+};
+
+export default App;
