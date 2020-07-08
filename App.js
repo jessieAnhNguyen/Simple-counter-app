7ba467e5744b9676db06bae66ddce3d2dfef8c52
Jessie Anh Nguyen <phuonganhnguyenthi26.3@gmail.com>
2020-07-08 14:18:35 +0700

    added more functions

--- App.js
+++ App.js
@@ -5,11 +5,20 @@ import Header from './components/Header';
 //simple incrementing button
 const App = () => {
   const [counter, setCounter] = useState(0);
+  function f1(counter) {
+    f2(counter);
+  }
+  function f2(counter) {
+    f3(counter);
+  }
+  function f3(counter) {
+    setCounter(counter + 1);
+  }
   return (
     <View>
       <Header title="Counting app" />
       <Text>{counter}</Text>
-      <Button title="Increment by 1" onPress={() => setCounter(counter + 1)} />
+      <Button title="Increment by 1" onPress={() => f1(counter)} />
       <Text>Current count is {counter}</Text>
     </View>
   );
